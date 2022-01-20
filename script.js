0c97903c5a611a8876284aee1d7a44fcf80f8971
Arimantas jurjonas <arimantaz@gmail.com>
2022-01-20 20:51:09 +0200

    third commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const x = 15
+ const y = 15
+ if (x >= y) {
+    alert ("Jegele");} else {
+        alert ("blogai");
+    }
+    if (x===y) {
+        alert ("Reiks taupyt!")
+    }
+// if ((x-5)< y) { alert ("blogai");}
+
+console.log ("operacija baigta");
