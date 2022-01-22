cccabd88edaf66c954dd11f8f0394bd94d5a180f
Arimantas jurjonas <arimantaz@gmail.com>
2022-01-22 12:36:24 +0200

    3uzduotistesinys commit

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // const vardas = promt ('koks tavo vardas?')
 // console.log (vardas);
 
-const pirmaverte = '8';
-console.log (pirmaverte)
-const gautaverte = Number (pirmaverte)
-console.log (gautaverte)
-const x = prompt ('pinigai');
+// const pirmaverte = '8';
+// console.log (pirmaverte)
+// const gautaverte = Number (pirmaverte)
+// console.log (gautaverte)
+// const x = prompt ('pinigai');
 
- const y = 15
- if (Number (x) > y) {
-    alert ("Jegele");} else if (Number (x)===y) {
-        alert ("reiks taupyt");
-    }
-    else {
-        alert ("Nieko gero!")
-    }
+//  const y = 15
+//  if (Number (x) > y) {
+//     alert ("Jegele");} else if (Number (x)===y) {
+//         alert ("reiks taupyt");
+//     }
+//     else {
+//         alert ("Nieko gero!")
+    // }
 // if ((x-5)< y) { alert ("blogai");}
 
-console.log ("operacija baigta");
+// console.log ("operacija baigta");
 // console.log (window)
 
 // function vardugeneratorius (name, surname) {
@@ -26,9 +26,15 @@ console.log ("operacija baigta");
 // const manopilnasvardas = vardugeneratorius ('Arimantas', 'Jurjonas')
 // console.log (manopilnasvardas)
 
-function min (a, b) {
-    if (a < b) {
-    return a;
-} return b;}
-const atsakymas = min(7, 4)
-console.log (atsakymas)
+// function min (a, b) {
+//     if (a < b) {
+//     return a;
+// } return b;}
+// const atsakymas = min(7, 4)
+// console.log (atsakymas)
+
+function sayHi (word) {
+
+     alert (word);
+}
+sayHi('laba diena');
