a78e3e0c35f97945de04a9f784b0e4fde1e2826d
Arimantas jurjonas <arimantaz@gmail.com>
2022-01-27 19:09:13 +0200

    ciklai

---
+++ scripts.js
@@ -0,0 +1,20 @@
+let kartukiekis = 0;
+
+// while (kartukiekis <= 1) {
+
+// console.log(kartukiekis)
+// kartukiekis++;
+// }
+
+do {
+console.log(kartukiekis)
+kartukiekis++;
+} while (kartukiekis < 3);
+
+ console.log("pabaiga");
+do {
+    console.log(kartukiekis)
+    kartukiekis--;
+    } while (kartukiekis > 0);
+    
+    console.log("end");   
